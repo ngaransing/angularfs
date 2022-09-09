5778068022baaded48bc73ac2b7a33057c822f09
Ngaransing KH <albert.asing@outlook.com>
2022-09-09 23:37:49 +0530

    lifecycle hook

---
+++ src/app/book/book.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Book } from '../types/Book';
+
+@Component({
+  selector: 'app-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.css']
+})
+export class BookComponent implements OnInit{
+
+  @Input() book: Book = {} as Book;
+
+  @Output() bookEmitter = new EventEmitter<Book>();
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void { 
+  }
+
+
+  addToCart(){
+    this.bookEmitter.emit(this.book);
+  }
+
+}
